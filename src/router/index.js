21b707cf63fb687052863d020f99f1edb388ffbf
doanduc2000 <doanminhducvip2000@gmail.com>
2023-03-29 15:30:20 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CustomerList from '../pages/CustomerList';
 import QuestionList from '../pages/QuestionList';
 import Report from '../pages/Report';
-export const URL = '/servey/servey-frontend';
+export const URL = '/servey';
 
 export const menu = [
   {
