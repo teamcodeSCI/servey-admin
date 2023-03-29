b4bfc365813af2c0399af22a509889f20ccbc1fa
doanduc2000 <doanminhducvip2000@gmail.com>
2023-03-29 15:31:21 +0700

    update

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,2 +1,2 @@
-// export const baseURL = 'https://app.scigroupvn.com/servey/servey-backend/public/api';
-export const baseURL = 'http://127.0.0.1:8000/api';
+export const baseURL = 'https://app.scigroupvn.com/servey/servey-backend/public/api';
+// export const baseURL = 'http://127.0.0.1:8000/api';
